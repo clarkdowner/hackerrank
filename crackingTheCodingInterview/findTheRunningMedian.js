49980a77b5e9a4abed9aaae177fca82d0da59b8c
clarkdowner <cddowner@gmail.com>
2017-01-09 14:32:08 -0800

    Naive solution implemented. Next: turn sortedInsert into binary insert

--- crackingTheCodingInterview/findTheRunningMedian.js
+++ crackingTheCodingInterview/findTheRunningMedian.js
@@ -1,8 +1,44 @@
 function main() {
-    var n = parseInt(readLine());
-    var a = [];
-    for(var a_i = 0; a_i < n; a_i++){
-       a[a_i] = parseInt(readLine());
-    }
+  var n = parseInt(readLine());
+  var a = [];
+  var sorted = [];
+  var even = true;
+  
 
+  // turn this into binarySearch for insert
+  var sortHelper = function(newElement, sortedArray) {
+    if (sortedArray.length === 0) {
+      return [newElement];
+    } else if (sortedArray[sortedArray.length - 1] < newElement) {
+      sortedArray.push(newElement);
+      return sortedArray;
+    } else {
+      for (var i = 0; i < sortedArray.length; i++) {
+	      if (sortedArray[i] > newElement) {
+          var first = sortedArray.slice(0, i);
+          var second = sortedArray.slice(i);
+          var newArray = first.push(newElement) + second;
+          return newArray;
+	      }
+      }
+    }      
+  }
+  
+  for(var a_i = 0; a_i < n; a_i++){
+    a[a_i] = parseInt(readLine());
+    sorted = sortHelper(a[a_i], sorted);
+    even = !even;
+    
+    if (even) {
+      var one = sorted[sorted.length / 2];
+      var two = sorted[(sorted.length / 2) - 1];
+      if (one + two % 2 === 0) {
+        console.log(one + two / 2 + '.0');
+      } else {
+        console.log((one + two) / 2);
+      }
+    } else {
+      console.log(sorted[Math.floor(sorted.length / 2)] + '.0');
+    }
+  }
 }
