2aecf6c6fb42f64b6ee3c0dc16579c2c1056e20e
clarkdowner <cddowner@gmail.com>
2017-01-10 19:04:20 -0800

    Contacts initialized

---
+++ crackingTheCodingInterview/contacts.js
@@ -0,0 +1,9 @@
+function main() {
+    var n = parseInt(readLine());
+    for(var a0 = 0; a0 < n; a0++){
+        var op_temp = readLine().split(' ');
+        var op = op_temp[0];
+        var contact = op_temp[1];
+    }
+
+}
