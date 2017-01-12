ab19f95efc2ae42efb35a5c3031726dfcb524c79
clarkdowner <cddowner@gmail.com>
2017-01-11 17:12:57 -0800

    CountingInversions initialized

---
+++ crackingTheCodingInterview/countingInversions.js
@@ -0,0 +1,9 @@
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n = parseInt(readLine());
+        arr = readLine().split(' ');
+        arr = arr.map(Number);
+    }
+
+}
