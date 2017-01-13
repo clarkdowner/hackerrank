6170785e659d74ab67f808162f2479616c2d4da2
clarkdowner <cddowner@gmail.com>
2017-01-12 17:58:33 -0800

    CountingInversions returns correct answer but times out for some solutions

--- crackingTheCodingInterview/countingInversions.js
+++ crackingTheCodingInterview/countingInversions.js
@@ -1,9 +1,35 @@
 function main() {
     var t = parseInt(readLine());
+    
+    var swapper = function(index, array) {
+      var temp = array[index];
+      array[index] = array[index + 1];
+      array[index + 1] = temp;
+        //console.log('indexes swapped: ' + index);
+    };
+    
+    var sortArray = function(array) {
+      var anySwaps = false;  
+      for (var i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          swapper(i, array);
+          numSwaps++;
+          anySwaps = true;
+        }  
+      }
+      if (anySwaps) {
+        sortArray(array);  
+      }  
+    };
+    
     for(var a0 = 0; a0 < t; a0++){
         var n = parseInt(readLine());
         arr = readLine().split(' ');
         arr = arr.map(Number);
+        var numSwaps = 0;
+        
+        sortArray(arr);
+        console.log(numSwaps);
     }
 
 }
