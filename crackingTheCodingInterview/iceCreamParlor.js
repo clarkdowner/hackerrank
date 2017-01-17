8c1c516b9fd55ee342aef3a37e43ee3122b9bdfd
clarkdowner <cddowner@gmail.com>
2017-01-16 16:51:37 -0800

    Ice Cream Parlor initialized

---
+++ crackingTheCodingInterview/iceCreamParlor.js
@@ -0,0 +1,12 @@
+function main() {
+  var t = parseInt(readLine());
+  for(var a0 = 0; a0 < t; a0++){
+  	// money
+    var m = parseInt(readLine());
+    // number of ice creams
+    var n = parseInt(readLine());
+    a = readLine().split(' ');
+    // array of costs
+    a = a.map(Number);
+  }
+}
