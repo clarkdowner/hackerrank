7e44aa28c32f4a4df7a4c389a0f1d8be74591feb
clarkdowner <cddowner@gmail.com>
2017-01-16 16:44:08 -0800

    Prefix Tree implemented but still receiving time outs

--- crackingTheCodingInterview/contacts.js
+++ crackingTheCodingInterview/contacts.js
@@ -5,6 +5,27 @@ function main() {
     var op_temp = readLine().split(' ');
     var op = op_temp[0];
     var contact = op_temp[1];
-  }
+    var currNode = prefixTree;
 
+    if (op === 'add') {
+      // add to prefix tree
+      for (var i = 0; i < contact.length; i++) {
+        var char = contact.charAt(i);
+        currNode[char] === undefined ? currNode[char] = [{}, 1] : currNode[char][1]++;
+        currNode = currNode[char];
+      }
+
+    } else {
+      // search prefix tree
+      for (var i = 0; i < contact.length; i++) {
+        var char = contact.charAt(i);
+        currNode = currNode[char];
+      }
+      // console results
+      currNode === undefined ? console.log(0) : console.log(currNode[1]);
+    }
+  }
 }
+
+
+// {[h: {[a: {[c: {}, 1]}, 1]}, 1]}
