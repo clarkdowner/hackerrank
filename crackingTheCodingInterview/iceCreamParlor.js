797574e36478acab8568fa74c25ad815b6cd60e7
clarkdowner <cddowner@gmail.com>
2017-01-16 17:14:47 -0800

    Stashing changes

--- crackingTheCodingInterview/iceCreamParlor.js
+++ crackingTheCodingInterview/iceCreamParlor.js
@@ -8,5 +8,19 @@ function main() {
     a = readLine().split(' ');
     // array of costs
     a = a.map(Number);
+
+
+    var hashStore = {};
+    var found = false;
+        
+    while (!found) {
+      for (var i = 0; i < a.length; i++) {
+        hashStore[a[i]] = [true, i + 1];
+        if (!found && a[i] !== m - a[i] && hashStore[m - a[i]] !== undefined) {
+        	console.log(Math.min(hashStore[a[i]][1], hashStore[m - a[i]][1]) + ' ' + Math.max(hashStore[a[i]][1], hashStore[m - a[i]][1]));
+        	found = true;
+        }
+      }   
+    }
   }
 }
