7895cfa16f806de979b736fbfec0c1c16be61cb9
clarkdowner <cddowner@gmail.com>
2017-01-16 16:19:05 -0800

    Starting with clean slate for contacts, will implement with prefix tree

--- crackingTheCodingInterview/contacts.js
+++ crackingTheCodingInterview/contacts.js
@@ -1,21 +1,10 @@
 function main() {
-    var n = parseInt(readLine());
-    var hashStore = {};
-    for(var a0 = 0; a0 < n; a0++){
-        var op_temp = readLine().split(' ');
-        var op = op_temp[0];
-        var contact = op_temp[1];
-        if (op === 'add') {
-            hashStore[contact] = contact;
-        } else {
-            var found = 0;
-            for (var key in hashStore) {
-                if (key.indexOf(contact) !== -1) {
-                    found++;
-                }
-            }
-            console.log(found);
-        }
-    }
+  var n = parseInt(readLine());
+  var prefixTree = {};
+  for(var a0 = 0; a0 < n; a0++){
+    var op_temp = readLine().split(' ');
+    var op = op_temp[0];
+    var contact = op_temp[1];
+  }
 
 }
