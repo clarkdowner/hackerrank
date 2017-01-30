d05a88f3cdd3182767ca8277da8959fae38ddfbd
clarkdowner <cddowner@gmail.com>
2017-01-30 12:56:35 -0800

    Functional solution but test cases too large for JavaScript

--- dataStructures/arrays/algorithmicCrush.js
+++ dataStructures/arrays/algorithmicCrush.js
@@ -1,3 +1,17 @@
 function processData(input) {
-    //Enter your code here
+  //Enter your code here
+  var input = input.split('\n')
+  for (var i = 0; i < input.length; i++) {
+      input[i] = input[i].split(' ');       
+  }
+  var buildArray = [];
+  for (var i = 0; i < input[0][0]; i++) {
+      buildArray.push(0);
+  }
+  for (var i = 1; i < input.length; i++) {
+      for (var j = input[i][0] - 1; j < input[i][1]; j++) {
+          buildArray[j] += +input[i][2];
+      }
+  }
+  console.log(Math.max(...buildArray));
 }
