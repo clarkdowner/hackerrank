57b940a60c6e202ff7e6a679e6a15725e3c193c2
clarkdowner <cddowner@gmail.com>
2017-02-06 09:16:08 -0800

    Logic implemented, timing out for longer tests

--- crackingTheCodingInterview/davisStaircase.js
+++ crackingTheCodingInterview/davisStaircase.js
@@ -2,5 +2,26 @@ function main() {
   var s = parseInt(readLine());
   for(var a0 = 0; a0 < s; a0++){
     var n = parseInt(readLine());
+
+    var stepCombos = 0;
+
+    var staircase = function(stairsRemaining) {
+      var poss = [3,2,1];
+
+      if (stairsRemaining === 0) {
+        stepCombos++;
+        return;
+      }
+
+      for (var i = 0; i < poss.length; i++) {
+        if (stairsRemaining - poss[i] >= 0) {
+          staircase(stairsRemaining - poss[i]);
+        }
+      }
+
+    }
+
+    staircase(n);
+    console.log(stepCombos);
   }
 }
