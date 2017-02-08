c56667d955200f9c00db89fb0b519c75e4793df9
clarkdowner <cddowner@gmail.com>
2017-02-08 13:37:19 -0800

    Binary Search implemented

---
+++ binarySearch.js
@@ -0,0 +1,25 @@
+var search = function(array, target) {
+   min = 0; // 4
+   max = array.length; // 5
+   
+   while (min <= max) {
+      var searchAt = Math.floor((max - min) / 2) + min; // 3, 
+      if (array[searchAt] === target) {
+         return searchAt;
+      } else if (array[searchAt] < target) {
+         min = searchAt + 1;
+      } else {
+         max = searchAt - 1;
+      }
+   }
+
+   return -1;
+}
+
+
+console.log(search([2, 4, 5, 9, 12, 17], 2)); // 0
+console.log(search([2, 4, 5, 9, 12, 17], 17)); // 5
+console.log(search([2, 4, 5, 9, 12, 17], 13));// -1
+console.log(search([2, 4, 5, 9, 12, 17], 1));// -1
+console.log(search([2, 4, 5, 9, 12, 17], 12));// 4
+console.log(search([2, 4, 5, 9, 12, 17], 9));// 3
