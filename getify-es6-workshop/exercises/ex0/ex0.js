a2ed182b31ec68a301075fa29814319195cd0ce3
clarkdowner <cddowner@gmail.com>
2017-02-23 13:46:57 -0800

    Translate ex0 functions to arrow functions

--- getify-es6-workshop/exercises/ex0/ex0.js
+++ getify-es6-workshop/exercises/ex0/ex0.js
@@ -1,7 +1,6 @@
-(function IIFE(){
+(() => {
 
-	function foo(x) {
-		var y = x * 2;
+	let foo = (x, y = x * 2) => {
 
 		return function bar(z) {
 			if (z.length > 3) {
@@ -9,30 +8,30 @@
 					if (v > 3) return v + y;
 					else return baz( v * 4 );
 				} );
+
 			}
 			else {
-				var obj = [];
+				let obj = [];
 
-				setTimeout( function bam(){
+				setTimeout( () => {
 					obj.length = 1;
 					obj[0] = this.w;
-				}.bind( this ), 100 );
+				}, 100 );
 
 				return obj;
 			}
 		};
 	}
 
-	var p = foo( 2 );
-	var list1 = [1,3,4];
-	var list2 = list1.concat( 6 );
+	let p = foo( 2 );
+	let list1 = [1,3,4];
+	let list2 = list1.concat( 6 );
 
 	list1 = p.call( { w: 42 }, list1 );
 	list2 = p( list2 );
 
-	setTimeout( function(){
-		console.log( list1[0] === list2.reduce( function(s,v){
-			return s + v;
-		}, 0 ) );
-	}, 200 );
+
+	setTimeout( () =>
+		console.log( list1[0] === list2.reduce( (s,v) => s + v, 0))
+	, 200);
 })();
