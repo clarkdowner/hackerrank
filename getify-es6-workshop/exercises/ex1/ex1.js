c0891748af34f84f6a4549bc3f99743113911686
clarkdowner <cddowner@gmail.com>
2017-02-23 16:31:42 -0800

    es6 exercise 1 complete

--- getify-es6-workshop/exercises/ex1/ex1.js
+++ getify-es6-workshop/exercises/ex1/ex1.js
@@ -1,12 +1,13 @@
-var x = 2, fns = [];
+const x = 2
+var fns = [];
 
-(function(){
-	var x = 5;
-
-	for (var i=0; i<x; i++) {
+{
+	let x = 5;
+	for (let i=0; i<x; i++) {
 		// ..
+		fns[i] = () => i
 	}
-})();
+}
 
 console.log(
 	(x * 2) === fns[x*2]()
