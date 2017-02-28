85eb291c522e07bc5983df20c4e570362a186321
clarkdowner <cddowner@gmail.com>
2017-02-27 21:23:44 -0800

    Decode String passes all test cases

--- decodeString.js
+++ decodeString.js
@@ -14,22 +14,47 @@ function decodeString(string) {
 	var poss = 1;
 
 	for (var i = 0; i < string.length; i++) {
-		// if (i < string.length - 1 && (string.charAt(i) === '1' || string.charAt(i) === '2')) {
-		// 	poss++;
-		// }
+
+		var char = string.charAt(i);
+
+		if (char === '0') {
+			if (i === 0) {
+				return false;
+			}
+			var prev = string.charAt(i - 1);
+			if (!(prev === '1' || prev === '2')) {
+				return false;
+			}
+			poss -= 2;
+		}
+
+		if (char === '1' && i !== string.length - 1) {
+			poss += 1;
+		}
+
+		if (char === '2' && i !== string.length - 1) {
+			var next = 0 + string.charAt(i + 1);
+			if (next < 7) {
+				poss += 1;
+			}
+		}
 	}
 
 	return poss;
 }
 
+function assert(a, b) {
+	if (a === b) { console.log('success'); }
+	else { console.log('expected ' + a + ' to equal ' + b); }
+}
 
-console.log(decodeString('121') === 3);
-console.log(decodeString('12') === 2);
-console.log(decodeString('1210') === 2);
-console.log(decodeString('0121') === false);
-console.log(decodeString('12001') === false);
-console.log(decodeString('120') === 1);
-console.log(decodeString('128') === 2);
-console.log(decodeString('1535') === 2);
-console.log(decodeString('1226') === 4);
-console.log(decodeString('30') === false);
+assert(decodeString('1210'), 2);
+assert(decodeString('120'), 1);
+assert(decodeString('121'), 3);
+assert(decodeString('12'), 2);
+assert(decodeString('0121'), false);
+assert(decodeString('12001'), false);
+assert(decodeString('128'), 2);
+assert(decodeString('1535'), 2);
+assert(decodeString('1226'), 4);
+assert(decodeString('30'), false);
