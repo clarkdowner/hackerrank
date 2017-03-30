05505840f3d28a027b566b8bc0da2817e2fd0b31
clarkdowner <cddowner@gmail.com>
2017-03-29 22:22:37 -0700

    ListSquared passes all tests

--- codewars/listSquared.js
+++ codewars/listSquared.js
@@ -16,5 +16,47 @@
 // list_squared(42, 250) --> [[42, 2500], [246, 84100]]
 
 function listSquared(m, n) {
-    // your code
+  var pairs = [];
+
+  function isPair(num) {
+    if (num === 1) {
+      return 1;
+    }
+
+    var sqRoot = Math.sqrt(num);
+    var divisors = [1, num];
+
+    if (Math.floor(sqRoot) === sqRoot) {
+      divisors.push(sqRoot);
+    }
+
+    for (var i = 2; i < sqRoot; i++) {
+      if (num % i === 0) {
+        divisors.push(i);
+        divisors.push(num / i);
+      }
+    }
+
+    var divisorSum = 0;
+    for (var i = 0; i < divisors.length; i++) {
+      divisorSum += Math.pow(divisors[i], 2);
+    }
+
+    return Math.floor(Math.sqrt(divisorSum)) === Math.sqrt(divisorSum) ? divisorSum : 0;
+  }
+
+  function addPair(a, b) {
+    pairs.push([a, b]);
+  }
+
+  for (var i = m; i <= n; i++) {
+    pairVal = isPair(i);
+    if (pairVal !== 0) {
+      addPair(i, pairVal);
+    }
+  }
+
+  return pairs;
 }
+
+console.log(listSquared(1,250));
