3f62b3118a57d370d796921252b0c24497f11d46
Clark Downer <cddowner@gmail.com>
2017-11-17 17:23:51 -0800

    Three sum naive solution O(n^3)

--- leetcode/three_sum.py
+++ leetcode/three_sum.py
@@ -19,25 +19,36 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        sols = []
-        s_hash = {}
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                key = nums[i] + nums[j]
-                if key not in s_hash:
-                    s_hash[key] = []
-                s_hash[key].append((i, j))
-        print(s_hash)
-        for i in range(len(nums)):
-            key = -1 * nums[i]
-            print(key)
-            if key in s_hash:
-                for x in s_hash[nums[i]]:
-                    if i not in x:
-                        sol = [nums[x[0]], nums[x[1]], nums[i]]
-                        sols.append(sol)
-
-        return sols
+        # sols = set()
+        # s_hash = {}
+        # for i in range(len(nums) - 1):
+        #     for j in range(i+1, len(nums)):
+        #         key = nums[i] + nums[j]
+        #         if key not in s_hash:
+        #             s_hash[key] = set()
+        #         s_hash[key].add((min(i, j), max(i, j),))
+        # print(s_hash)
+        # for i in range(len(nums)):
+        #     key = -1 * nums[i]
+        #     if key in s_hash:
+        #         print('key is: ', key)
+        #         for x in s_hash[nums[i]]:
+        #             print('hashes: ', nums[x[0]], nums[x[1]])
+        #             if i not in x:
+        #                 sol = tuple(sorted([nums[x[0]], nums[x[1]], nums[i]]))
+        #                 sols.add(sol)
+        #
+        # return [list(x) for x in sols]
+        solutions = set()
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        vals = [nums[i], nums[j], nums[k]]
+                        vals.sort()
+                        solutions.add(tuple(vals))
+        return [list(x) for x in solutions]
 
 s = Solution()
+# print(s.threeSum([-1, 0, -1]))
 print(s.threeSum([-1, 0, 1, 2, -1, -4]))
