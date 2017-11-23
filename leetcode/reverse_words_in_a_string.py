114b78cab3b639ef36fd3f76ce70cde6709d74f4
clarkdowner <cddowner@gmail.com>
2017-11-23 01:13:19 -0800

    Reverse words in a string solution

---
+++ leetcode/reverse_words_in_a_string.py
@@ -0,0 +1,18 @@
+"""
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+"""
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        spl = s.split(' ')
+        rev = list(reversed(spl))
+        return ' '.join(rev)
