97bd372eb1cdeab5b7bf64e4475e7e735d427af8
Clark Downer <cddowner@gmail.com>
2018-01-17 16:14:56 -0800

    Count characters solution

---
+++ codewars/count_characters_in_your_string.py
@@ -0,0 +1,16 @@
+"""
+The main idea is to count all the occuring characters(UTF-8) in string. If you have string like this aba then the result
+should be { 'a': 2, 'b': 1 }
+
+What if the string is empty ? Then the result should be empty object literal { }
+"""
+
+
+def count(string):
+    map = {}
+    for char in string:
+        if char in map:
+            map[char] += 1
+        else:
+            map[char] = 1
+    return map
