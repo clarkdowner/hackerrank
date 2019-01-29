c9a299281853e661b46836dd15e125c8507672ec
Clark Downer <cddowner@gmail.com>
2019-01-29 15:04:29 -0800

    Minimum swaps to sort solution.

---
+++ geeks_for_geeks/sorting/minimum_swaps_to_sort.py
@@ -0,0 +1,74 @@
+"""
+Given an array of N distinct elementsA[ ], find the minimum number of swaps required to sort the array.Your are required to complete the function which returns an integer denoting the minimum number of swaps, required to sort the array.
+
+Examples:
+
+Input : {4, 3, 2, 1}
+Output : 2
+Explanation : Swap index 0 with 3 and 1 with 2 to
+              form the sorted array {1, 2, 3, 4}.
+
+Input : {1, 5, 4, 3, 2}
+Output : 2
+
+Input:
+The first line of input contains an integer T denoting the no of test cases . Then T test cases follow . Each test case contains an integer N denoting the no of element of the array A[ ]. In the next line are N space separated values of the array A[ ] .
+
+Output:
+For each test case in a new line output will be an integer denoting  minimum umber of swaps that are  required to sort the array.
+
+Constraints:
+1<=T<=100
+1<=N<=100
+1<=A[] <=1000
+
+Example(To be used only for expected output):
+Input:
+2
+4
+4 3 2 1
+5
+1 5 4 3 2
+Output:
+2
+2
+"""
+
+
+{
+if __name__ == '__main__':
+    t = int(input())
+for i in range(t):
+    n = int(input())
+arr = list(map(int, input().strip().split()))
+print(minSwaps(arr, n))
+# Contributed by: Harshit Sidhwa
+
+}
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+
+# Your task is to complete this function
+# function should return an integer denoting the minimum number of swap's
+def minSwaps(arr, n):
+    match_to = sorted(arr)
+    swaps = 0
+
+    for i in range(len(arr)):
+        current_set = {i}
+        next_idx = match_to.index(arr[i])
+
+        while next_idx not in current_set:
+            current_set.add(next_idx)
+            next_idx = match_to.index(arr[next_idx])
+
+        for idx in current_set:
+            arr[idx] = match_to[idx]
+
+        swaps += len(current_set) - 1
+        current_set.clear()
+
+    return swaps
