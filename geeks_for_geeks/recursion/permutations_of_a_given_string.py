866e6dfe624f021d5f47578f2f2ee104cc34b14c
Clark Downer <cddowner@gmail.com>
2019-02-05 20:14:24 -0500

    Permutations of a given string recursive solution.

--- geeks_for_geeks/recursion/permutations_of_a_given_string.py
+++ geeks_for_geeks/recursion/permutations_of_a_given_string.py
@@ -7,10 +7,6 @@ The first line of input contains an integer T, denoting the number of test cases
 Output:
 For each test case, print all permutations of a given string S with single space and all permutations should be in lexicographically increasing order.
 
-Constraints:
-1 ≤ T ≤ 10
-1 ≤ size of string ≤ 5
-
 Example:
 Input:
 2
@@ -21,7 +17,20 @@ Output:
 ABC ACB BAC BCA CAB CBA
 ABGS ABSG AGBS AGSB ASBG ASGB BAGS BASG BGAS BGSA BSAG BSGA GABS GASB GBAS GBSA GSAB GSBA SABG SAGB SBAG SBGA SGAB SGBA
 """
-import itertools
+
+
+def get_perms(remaining_string, existing_string=''):
+    if len(remaining_string) == 0:
+        return [existing_string]
+
+    perms = list()
+    for index, letter in enumerate(remaining_string):
+        next_existing = existing_string + letter
+        next_remaining = remaining_string[:index] + remaining_string[index + 1:]
+
+        perms.extend(get_perms(next_remaining, next_existing))
+
+    return perms
 
 
 if __name__ == '__main__':
@@ -30,5 +39,5 @@ if __name__ == '__main__':
 
     for _ in range(test_cases):
         string = input()
-        perms = [''.join(x) for x in itertools.permutations(string)]
+        perms = get_perms(string)
         # print(*sorted(perms))
